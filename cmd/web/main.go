8cf8d9e3dc19260e522c3ac6fa0bf6525ad1ba5d
Stephen Montague <stephen.d.montague@gmail.com>
2021-11-12 11:21:52 -0500

    Further breakdown of files into appropriate directories

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/stephenmontague/hello-world-go/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 // main is the main application function
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	fmt.Printf("Starting application on port %s", portNumber)
 
 	_ = http.ListenAndServe(portNumber, nil) // This is how we start a webserver in Go - Ignoring the error that comes back if there is one
 }
