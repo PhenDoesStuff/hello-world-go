65f60e4348b5e43852aadfa124a75aea720018ef
Stephen Montague <me@montaguestephen.com>
2021-11-12 07:16:59 -0500

    Serving Hello, world! to a web server on port 8080 of local host

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		n, err := fmt.Fprintf(w, "Hello, world!") // Fprintf returns a number of bytes written and error, so we have to do something with them (see below)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n)) // Sprintf allows for us to add different types into print statements using %d
+	})
+
+	_ = http.ListenAndServe(":8080", nil) // This is how we start a webserver in Go - Ignoring the error that comes back if there is one
 }
