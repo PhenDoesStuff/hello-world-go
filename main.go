e9471b6ad10b741a68e53caf7d9d22076a5a597c
Stephen Montague <me@montaguestephen.com>
2021-11-12 07:33:11 -0500

    Enhancing our web server

--- main.go
+++ main.go
@@ -5,14 +5,30 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		n, err := fmt.Fprintf(w, "Hello, world!") // Fprintf returns a number of bytes written and error, so we have to do something with them (see below)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n)) // Sprintf allows for us to add different types into print statements using %d
-	})
+const portNumber = ":8080"
 
-	_ = http.ListenAndServe(":8080", nil) // This is how we start a webserver in Go - Ignoring the error that comes back if there is one
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(1, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page, and 1 + 2 is %d", sum))
+}
+
+// addValues adds two integers and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main application function
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+fmt.Println(fmt.Sprintf("Starting application onf port %s", portNumber))
+
+	_ = http.ListenAndServe(portNumber, nil) // This is how we start a webserver in Go - Ignoring the error that comes back if there is one
 }
