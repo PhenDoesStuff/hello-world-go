03610cb1cc33f0f23c4757e4b83ddbae4a9a92df
Stephen Montague <stephen.d.montague@gmail.com>
2021-11-15 18:25:31 -0500

    Making the creation of the template cache a little easier and more efficient.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/stephenmontague/hello-world-go/pkg/config"
 	"github.com/stephenmontague/hello-world-go/pkg/handlers"
+	"github.com/stephenmontague/hello-world-go/pkg/render"
 )
 
-const portNumber = ":8080"
+const portNumber = ":8081"
 
 // main is the main application function
 func main() {
+	var app config.AppConfig
+	
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
