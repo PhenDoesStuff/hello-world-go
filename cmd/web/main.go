498298f1a92390c6611a63596998929db5a1c15d
Stephen Montague <me@montaguestephen.com>
2021-11-17 07:50:47 -0500

    Updating our project to be able to use a session

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,17 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/stephenmontague/hello-world-go/pkg/config"
 	"github.com/stephenmontague/hello-world-go/pkg/handlers"
 	"github.com/stephenmontague/hello-world-go/pkg/render"
 )
 
 const portNumber = ":8080"
+var app config.AppConfig // Setting this outside of the main package gives any package part of main access to it
+var session *scs.SessionManager // Same as ^
 
 // main is the main application function
-func main() {
-	var app config.AppConfig
+func main() {	
+	// change this to true when in production
+	app.InProduction = false
+
+	// SCS is a package that helps us set up a session so people aren't logged out immediately
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction // In production we want this to be true. False for dev b/c localhost is not secure
+
+	app.Session = session
 	
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -3,3 +3,7 @@ module github.com/stephenmontague/hello-world-go
 go 1.17
 
 require github.com/go-chi/chi/v5 v5.0.6
+
+require github.com/justinas/nosurf v1.1.1
+
+require github.com/alexedwards/scs/v2 v2.4.0
